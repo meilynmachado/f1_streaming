eb96188e1915e9471571b68efb193edec36652ed
Meilyn Barbosa <meilynbarbosa@gmail.com>
2025-08-05 13:53:16 -0400

    feat: adding dashboard component

---
+++ f1_streaming_frontend/src/app/dashboard/dashboard.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.html',
+  imports: [CommonModule]
+})
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.html',
+  styleUrls: ['./dashboard.scss']
+})
+export class Dashboard {
+  showMenu = false;
+  
+  // Dados fictícios para o dashboard
+  user = { name: 'Carlos Sainz' };
+  
+  seasonStats = {
+    fastestLap: '1:12.345',
+    averageLap: '1:15.678',
+    lastWinner: 'Max Verstappen'
+  };
+
+  teamRanking = [
+    { position: 1, name: 'McLaren', points: 460 },
+    { position: 2, name: 'Ferrari', points: 222 },
+    { position: 3, name: 'Mercedes', points: 210 }
+  ];
+
+  lastRaces = [
+    { gp: 'Canadá', date: '09/06/2024', team: 'Mercedes', bestLap: '1:20.161' },
+    { gp: 'Mônaco', date: '26/05/2024', team: 'Ferrari', bestLap: '1:13.556' }
+  ];
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
+
+  refreshData() {
+
+    this.seasonStats.fastestLap = '1:11.890';
+    this.teamRanking[0].points = 480;
+    this.lastRaces.unshift({
+      gp: 'França', 
+      date: '23/06/2024', 
+      team: 'Red Bull', 
+      bestLap: '1:12.402'
+    });
+  }
+
+  getSeasonStats() {
+  return [
+    { title: 'Volta mais rápida', value: this.seasonStats.fastestLap },
+    { title: 'Média de tempo por volta', value: this.seasonStats.averageLap },
+    { title: 'Vencedor da última corrida', value: this.seasonStats.lastWinner }
+  ];
+}
+
+getMedalType(position: number): string {
+  switch(position) {
+    case 1: return 'gold';
+    case 2: return 'silver';
+    case 3: return 'bronze';
+    default: return '';
+  }
+}
+}
